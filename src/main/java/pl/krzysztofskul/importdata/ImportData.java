55e67dbe61c3c117f34c54f95380e27aa8d1dde9
krzysztofskul <krzysztofskul@protonmail.com>
2022-10-28 22:50:44 +0200

    #3.1.2
    + import calculation files

--- src/main/java/pl/krzysztofskul/importdata/ImportData.java
+++ src/main/java/pl/krzysztofskul/importdata/ImportData.java
@@ -1,12 +1,54 @@
 package pl.krzysztofskul.importdata;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
+//Singleton
 public class ImportData {
+	
+	/*
+	 * PARAMS.
+	 */
+	
+	private static ImportData importData;
+	private String pathProjectsToImport = "D:\\SMNSH\\karta_projektu\\Projekty";
+	
+	/*
+	 * CONSTR.
+	 */
+	
+	private ImportData() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-	public List<String> importFolderNames(String path) {
+	public static ImportData getImportDataSingleton() {
+		if (importData == null) {
+			importData = new ImportData();
+		}
+		
+		return importData;
+	}
+
+	/*
+	 * GETTERS AND SETTERS
+	 */
+	
+	public String getPathProjectsToImport() {
+		return pathProjectsToImport;
+	}
+
+	public void setPathProjectsToImport(String pathProjectsToImport) {
+		this.pathProjectsToImport = pathProjectsToImport;
+	}
+
+	/*
+	 * METHODS
+	 */
+	
+	public List<String> importProjectsFolderNames(String path) {
 		List<String> folderNameList = new ArrayList<String>();
 		
 		File rootFolder = new File(path);
@@ -20,4 +62,31 @@ public class ImportData {
 		return folderNameList;
 	}
 	
+	public List<String> getCalculationFilesFullPath() /* throws FileNotFoundException */ {
+		
+		List<String> pathsToCalculationFiles = new ArrayList<String>();
+		
+			for (String pathToProjectFolder : this.importProjectsFolderNames(ImportData.getImportDataSingleton().pathProjectsToImport)) {
+				
+				/*
+				 * declaring folder with sls documentation
+				 */
+				File folderSlsDoc = new File(ImportData.getImportDataSingleton().pathProjectsToImport+"//"+pathToProjectFolder+"//01. Dokumenty_SLS KFP"); // project folder
+				File[] files = folderSlsDoc.listFiles();
+				
+				/*
+				 * searching calculation file
+				 */
+				for (File file : files) {
+					if (file.getName().contains("Kalkulacja") && file.getName().contains(".xls")) {
+						pathsToCalculationFiles.add(file.getPath());
+						break;
+					}
+				}
+				
+			}
+		return pathsToCalculationFiles;
+		
+
+	}
 }
--- src/test/java/pl/krzysztofskul/importdata/ImportDataTest.java
+++ src/test/java/pl/krzysztofskul/importdata/ImportDataTest.java
@@ -7,13 +7,29 @@ import org.junit.Test;
 public class ImportDataTest {
 
 	@Test
-	public void test() {
-		assertTrue(new ImportData().importFolderNames("D:\\SMNSH\\karta_projektu\\test").size() > 0);
+	public void test01() {
+		assertTrue(ImportData.getImportDataSingleton().importProjectsFolderNames("D:\\SMNSH\\karta_projektu\\test").size() > 0);
 		
-		for (String name : new ImportData().importFolderNames("D:\\SMNSH\\karta_projektu\\test")) {
+		for (String name : ImportData.getImportDataSingleton().importProjectsFolderNames("D:\\SMNSH\\karta_projektu\\test")) {
 			System.out.println(name);
 		}
 		
+	}
+	
+	@Test
+	public void test02() {
+		assertTrue(ImportData.getImportDataSingleton().importProjectsFolderNames(ImportData.getImportDataSingleton().getPathProjectsToImport()).size() > 0);
+		
+		for (String name : ImportData.getImportDataSingleton().importProjectsFolderNames(ImportData.getImportDataSingleton().getPathProjectsToImport())) {
+			System.out.println(name);
+		}
+		
+	}
+	
+	@Test
+	public void test03() {
+		assertTrue(ImportData.getImportDataSingleton().getCalculationFilesFullPath().size() > 0);
+		System.out.println(ImportData.getImportDataSingleton().getCalculationFilesFullPath().toString());
 	}
 
 }
